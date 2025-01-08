d50d2dd5ce5ee462fd2b991bad4348f19149bd2c
Tiffany Yang <121731114+Vianne-a1@users.noreply.github.com>
2025-01-08 19:59:53 +0000

    lib

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <curses.h>
